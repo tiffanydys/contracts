0f02ef0d2e0e54f0f24875556844729d8a078b6c
Adam Hannigan <adam@payble.com.au>
2022-02-04 19:03:34 -0300

    [FEAT] Refactor events into object params with timestamp tests

---
+++ src/domain/game/game.test.ts
@@ -0,0 +1,89 @@
+import Decimal from "decimal.js-light";
+import { INITIAL_FARM } from "./lib/constants";
+import { processActions } from "./game";
+
+describe("game", () => {
+  it("processes an event", () => {
+    const state = processActions(INITIAL_FARM, [
+      { type: "item.harvested", index: 0, createdAt: Date.now() },
+    ]);
+
+    expect(state.inventory.Sunflower).toEqual(new Decimal(1));
+    expect(state.fields[0]).toBeUndefined();
+  });
+
+  it("processes multiple events", () => {
+    const state = processActions(
+      { ...INITIAL_FARM, inventory: { "Sunflower Seed": new Decimal(1) } },
+      [
+        {
+          type: "item.planted",
+          index: 4,
+          item: "Sunflower Seed",
+          createdAt: Date.now() - 60 * 1000,
+        },
+        {
+          type: "item.harvested",
+          index: 4,
+          createdAt: Date.now(),
+        },
+      ]
+    );
+
+    expect(state.inventory.Sunflower).toEqual(new Decimal(1));
+  });
+
+  it("ensures events are in order", () => {
+    expect(() =>
+      processActions(
+        { ...INITIAL_FARM, inventory: { "Sunflower Seed": new Decimal(1) } },
+        [
+          {
+            type: "item.planted",
+            index: 4,
+            item: "Sunflower Seed",
+            createdAt: Date.now(),
+          },
+          {
+            type: "item.harvested",
+            index: 4,
+            createdAt: Date.now() - 60 * 1000,
+          },
+        ]
+      )
+    ).toThrow("Events must be in chronological order");
+  });
+
+  it("ensures events are not in the future", () => {
+    expect(() =>
+      processActions(
+        { ...INITIAL_FARM, inventory: { "Sunflower Seed": new Decimal(1) } },
+        [
+          {
+            type: "item.planted",
+            index: 4,
+            item: "Sunflower Seed",
+            createdAt: Date.now() + 5,
+          },
+        ]
+      )
+    ).toThrow("Event cannot be in the future");
+  });
+
+  it("ensures events are not in the past", () => {
+    expect(() =>
+      processActions(
+        { ...INITIAL_FARM, inventory: { "Sunflower Seed": new Decimal(1) } },
+        [
+          {
+            type: "item.planted",
+            index: 4,
+            item: "Sunflower Seed",
+            // 10 minutes ago
+            createdAt: Date.now() - 60 * 10 * 1000,
+          },
+        ]
+      )
+    ).toThrow("Event is too old");
+  });
+});
--- src/domain/game/game.ts
+++ src/domain/game/game.ts
@@ -1,15 +1,52 @@
 import Decimal from "decimal.js-light";
 import { getFarmsByAccount, updateFarm } from "../../repository/farms";
-import { GameEvent, processEvent } from "./events";
+import { EVENTS, GameEvent } from "./events";
 import { GameState, InventoryItemName } from "./types/game";
 
-function processActions(state: GameState, actions: GameEvent[]) {
+type GameAction = GameEvent & {
+  createdAt: number;
+};
+
+function processEvent(state: GameState, action: GameAction): GameState {
+  const handler = EVENTS[action.type];
+
+  if (!handler) {
+    throw new Error(`Unknown event type: ${action}`);
+  }
+
+  return handler({
+    state,
+    createdAt: action.createdAt,
+    // TODO - fix this type error
+    action: action as never,
+  });
+}
+
+// An event must be saved within 5 minutes before it is considered stale
+const MINUTES_TO_SAVE = 5;
+
+export function processActions(state: GameState, actions: GameAction[]) {
   // Validate actions
   if (!Array.isArray(actions)) {
     throw new Error("Expected actions to be an array");
   }
 
-  return actions.reduce((farm, action) => {
+  return actions.reduce((farm, action, index) => {
+    if (index > 0) {
+      const previousAction = actions[index - 1];
+      if (previousAction.createdAt > action.createdAt) {
+        throw new Error("Events must be in chronological order");
+      }
+    }
+
+    if (action.createdAt > Date.now()) {
+      throw new Error("Event cannot be in the future");
+    }
+
+    if (action.createdAt < Date.now() - MINUTES_TO_SAVE * 60 * 1000) {
+      throw new Error("Event is too old");
+    }
+
     return processEvent(farm, action);
   }, state);
 }
@@ -17,15 +54,15 @@ function processActions(state: GameState, actions: GameEvent[]) {
 type SaveArgs = {
   farmId: number;
   account: string;
-  actions: GameEvent[];
+  actions: GameAction[];
 };
+
 export async function save({ farmId, account, actions }: SaveArgs) {
   const farms = await getFarmsByAccount(account);
   const farm = farms.find((f) => f.id === farmId);
   if (!farm) {
     throw new Error("Farm does not exist!");
   }
-  console.log({ farm });
 
   // Reuse deserialization
 
