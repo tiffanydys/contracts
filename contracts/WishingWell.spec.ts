418f1cc416e48c5d18f345a8c71ea449e213245b
Adam Hannigan <adam@payble.com.au>
2022-02-09 20:47:20 -0300

    Add WishinWell contract

---
+++ contracts/WishingWell.spec.ts
@@ -0,0 +1,23 @@
+import Web3 from "web3";
+import { soliditySha3 } from "web3-utils";
+import { deploySFLContracts, gasLimit, TestAccount } from "./test-support";
+
+describe("Wishing Well contract", () => {
+  it("throws tokens into the well", () => {});
+
+  it("ensures a user has tokens", () => {});
+
+  it("enables a user to withdraw tokens after 3 days", () => {});
+
+  it("ensures tokens have been in the well for 3 days before receiving reward", () => {});
+
+  it("rewards a user proportionally", () => {});
+
+  it("rewards a user over multiple days", () => {});
+
+  it("ensure user has not been rewarded in last 3 days before pulling out tokens", () => {});
+
+  it("only pulls out what was put in", () => {});
+
+  it("pulls out tokens from well", () => {});
+});
