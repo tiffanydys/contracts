64ce09f4785e5f4bd294e79f68641da32468ad97
Adam Hannigan <adam@payble.com.au>
2022-02-15 18:13:47 -0300

    Increase items crafted

--- src/api/autosave.ts
+++ src/api/autosave.ts
@@ -20,7 +20,7 @@ const schema = () =>
           Joi.object({
             type: Joi.string().equal("item.crafted"),
             item: Joi.string(),
-            amount: Joi.number().min(1).max(100).integer(),
+            amount: Joi.number().min(1).max(1000).integer(),
             createdAt: eventTimeValidation(),
           }),
           Joi.object({
