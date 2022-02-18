e4486b329edb3e8eeb8b6e91e8ac669c794e8dc7
Adam Hannigan <adam@payble.com.au>
2022-02-18 12:59:49 -0300

    [FIX] Metadata URL

--- src/api/metadata.ts
+++ src/api/metadata.ts
@@ -10,8 +10,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      description: `Land which is used to play at Sunflower Land. You can visit this farm at https://sunflower-land.com/play?farmId=${id}`,
-      external_url: `https://sunflower-land.com/play?farmId=${id}`,
+      description: `Land which is used to play at Sunflower Land. You can visit this farm at https://sunflower-land.com/play/?farmId=${id}`,
+      external_url: `https://sunflower-land.com/play/?farmId=${id}`,
       image: "https://sunflower-land.com/testnet/farms/farm.png",
       name: `Sunflower Land #${id}`,
       attributes: [],
