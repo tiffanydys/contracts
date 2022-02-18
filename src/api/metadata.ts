9d09b8f005f46325b8f4a618deff6294c69db795
Adam Hannigan <adam@payble.com.au>
2022-02-18 10:02:37 -0300

    [CHORE] Update sync whitelist

--- src/api/metadata.ts
+++ src/api/metadata.ts
@@ -10,9 +10,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      description: `Land which is used to play at Sunflower Land. You can visit this farm at https://sunflower-land.com/play?id=${id}`,
-      external_url: `https://sunflower-land.com/play?id=${id}`,
-      //image: "https://sunflower-land.com/play/nfts/farm.png",
+      description: `Land which is used to play at Sunflower Land. You can visit this farm at https://sunflower-land.com/play?farmId=${id}`,
+      external_url: `https://sunflower-land.com/play?farmId=${id}`,
       image: "https://sunflower-land.com/testnet/farms/farm.png",
       name: `Sunflower Land #${id}`,
       attributes: [],
