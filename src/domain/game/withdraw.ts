271d7c71cac407b87d4bc5c8f0f2058e9ed531f8
Adam Hannigan <adam@payble.com.au>
2022-03-05 07:27:11 -0300

    [FIX] Validation on withdraw

--- src/domain/game/withdraw.ts
+++ src/domain/game/withdraw.ts
@@ -1,10 +1,11 @@
 import Decimal from "decimal.js-light";
-import { fromWei } from "web3-utils";
+import { fromWei, toWei } from "web3-utils";
 import { storeWithdraw } from "../../repository/eventStore";
 import { getFarmById } from "../../repository/farms";
 
 import { withdrawSignature } from "../../services/web3/signatures";
 import { isBlackListed } from "./lib/blacklist";
+import { makeGame } from "./lib/transforms";
 
 /**
  * Returns the tax rate when withdrawing SFL
@@ -64,11 +65,19 @@ export async function withdraw({
     throw new Error(`Farm #${farmId} - ${farm.updatedBy} is blacklisted`);
   }
 
+  const balance = new Decimal(toWei(farm.gameState.balance));
+  if (balance.lessThan(sfl)) {
+    throw new Error("Not enough SFL");
+  }
+
+  // TODO inventory validation in future - smart contract ERC20 protocols won't enable this though
+
   // Smart contract does balance validation so don't worry about it here
   const signature = await withdrawSignature({
     sender: sender,
     farmId: farmId,
-    sessionId: sessionId,
+    // Always make sure we don't use what they pass us
+    sessionId: farm.sessionId,
     sfl: sfl,
     ids: ids,
     amounts: amounts,
@@ -77,7 +86,7 @@ export async function withdraw({
 
   await storeWithdraw({
     account: sender,
-    sessionId,
+    sessionId: farm.sessionId,
     farmId,
     sfl,
     ids,
