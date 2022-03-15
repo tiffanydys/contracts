91cd4661c6810a30bff08e5573ac1088afd57bcb
Adam Hannigan <adam@payble.com.au>
2022-03-15 17:43:11 -0300

    [FIX] Remove duplicate open sea URL

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -65,7 +65,6 @@ export default class ApiStack extends sst.Stack {
           "http://localhost:3000",
           "https://sunflower-land.com",
           "https://www.sunflower-land.com",
-          "https://www.sunflower-land.com",
           "https://opensea.io",
         ],
         allowMethods: [
