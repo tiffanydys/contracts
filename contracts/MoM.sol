8d3f7cce107c9c4ef9fc8d9cf2d02d284f2a2e8e
Adam Hannigan <adamhannigan81@gmail.com>
2022-05-23 09:28:38 +1000

    Burn engine core

--- contracts/MoM.sol
+++ contracts/MoM.sol
@@ -5,15 +5,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import "./Inventory.sol";
+import "./Farm.sol";
 import "./GameOwner.sol";
 
 contract MoMNFT is ERC721Enumerable, GameOwner  {
     SunflowerLandInventory inventory;
+    SunflowerLand farm;
+
     // Inclusive of items traded (burnt) for Sunflower Land items
     uint private supply = 0;
 
-    constructor(SunflowerLandInventory _inventory) ERC721("MoM Sunflower Crossover", "MSF")  {
+    constructor(SunflowerLandInventory _inventory, SunflowerLand _farm) ERC721("MoM Sunflower Crossover", "MSF")  {
         inventory = _inventory;
+        farm = _farm;
     }
 
     function destroy() public onlyOwner payable {
@@ -29,19 +33,38 @@ contract MoMNFT is ERC721Enumerable, GameOwner  {
 
     }
 
-    function trade(uint tokenId) public {
+    function trade(uint tokenId, uint farmId) public {
         require(balanceOf(_msgSender()) > 0, "MoMNFT: Player does not have NFT to trade");
         require(ownerOf(tokenId) == _msgSender(), "MoMNFT: You are not the owner");
 
         _burn(tokenId);
 
+        // Get the holding address of the farm
+        Farm memory farmNFT = farm.getFarm(farmId);
+
+        // Check they own the farm
+        require(
+            farmNFT.owner == _msgSender(),
+            "SunflowerLand: You do not own this farm"
+        );
+        
+        // Burn the rocket repair
+        uint256[] memory burnIds = new uint256[](1);
+        uint256[] memory burnAmounts = new uint256[](1);
+
+        // Intialises the only element in the array
+        burnIds[0] = 910;
+        burnAmounts[0] = 1;
+        inventory.gameBurn(farmNFT.account, burnIds, burnAmounts);
+
+        // Mint the observatory
         uint256[] memory mintIds = new uint256[](1);
         uint256[] memory mintAmounts = new uint256[](1);
         // Intialises the only element in the array
         mintIds[0] = 911;
         mintAmounts[0] = 1;
         
-        inventory.gameMint(_msgSender(), mintIds, mintAmounts, "");
+        inventory.gameMint(farmNFT.account, mintIds, mintAmounts, "");
     }
 
     function transferFrom(
